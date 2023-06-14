1907a027d891cf1314f2b2beafb8cfa174e457d6
Tiasha Mondal <tiashamondal29@gmail.com>
2023-06-14 16:20:03 +0100

    create pipeline

--- src/pipeline/predict_pipeline.py
+++ src/pipeline/predict_pipeline.py
@@ -1,11 +1,23 @@
 import sys
 import pandas as pd
 from src.exception import CustomException
-from src.utils import load_object 
+from src.utils import load_model
 
 class PredictPipeline:
     def __init__(self) -> None:
         pass
+    def predict(self, features):
+        try:
+            model_path='artifacts/model.pkl'
+            preprocessor_path='artifacts/proprocessor.pkl'
+            preprocessor=load_model(file_path=preprocessor_path)
+            model=load_model(file_path=model_path)
+            data_scaled=preprocessor.transform(features)
+            preds=model.predict(data_scaled)
+            return preds
+        except Exception as e:
+            raise CustomException(e, sys)
+        
 
 # This class is responsible for mapping all the inputes got from UI to the backend with perticular values
 class CustomData:
@@ -31,20 +43,21 @@ class CustomData:
 
         self.writing_score = writing_score
 
-# This will return all my data in the form of a dataframe
-def get_data_as_data_frame(self):
-    try:
-        custom_data_input_dict= {
-           "gender": [self.gender],
-                "race_ethnicity": [self.race_ethnicity],
-                "parental_level_of_education": [self.parental_level_of_education],
-                "lunch": [self.lunch],
-                "test_preparation_course": [self.test_preparation_course],
-                "reading_score": [self.reading_score],
-                "writing_score": [self.writing_score],
-        }
+    # This will return all my data in the form of a dataframe
+    def get_data_as_data_frame(self):
+        try:
+            # Whatever inputs I am giving on my web application is getting mapped with these particular values and then creates a dataframe out of it
+            custom_data_input_dict= {
+                    "gender": [self.gender],
+                    "race_ethnicity": [self.race_ethnicity],
+                    "parental_level_of_education": [self.parental_level_of_education],
+                    "lunch": [self.lunch],
+                    "test_preparation_course": [self.test_preparation_course],
+                    "reading_score": [self.reading_score],
+                    "writing_score": [self.writing_score],
+            }
 
-        return pd.DataFrame(custom_data_input_dict)
-    except Exception as e:
-        raise CustomException(e, sys)
+            return pd.DataFrame(custom_data_input_dict)
+        except Exception as e:
+            raise CustomException(e, sys)
 
